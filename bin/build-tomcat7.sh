9d4629e2ac45db87e9df796d8c26fe7c86c9c8c2
Kensuke ISHIDA <kensuke@dreamarts.co.jp>
2019-04-15 10:58:23 +0900

    build tar.gz module with original binary

--- bin/build-tomcat7.sh
+++ bin/build-tomcat7.sh
@@ -4,13 +4,27 @@ export JAVA_HOME=/usr/lib/jvm/zulu-7-amd64
 export ANT_HOME=/work/apache-ant
 export PATH=$PATH:${ANT_HOME}/bin
 
+if [ "x$BRANCH" = "x" ]; then
+    export BRANCH=7.0.94-p1
+fi
+if [ "x$VERSION" = "x" ]; then
+    export VERSION=7.0.94
+fi
+
+ORIGINAL_BINARY="http://ftp.jaist.ac.jp/pub/apache/tomcat/tomcat-7/v${VERSION}/bin/apache-tomcat-${VERSION}.tar.gz"
+
 mkdir -p /work &&
 cd /work &&
+rm -rf apache-tomcat-${VERSION} &&
+rm -f apache-tomcat-${VERSION}.tar.gz &&
+wget $ORIGINAL_BINARY &&
+tar zxvf apache-tomcat-${VERSION}.tar.gz  &&
 rm -rf tomcat &&
-git clone -b 7.0.x --depth 1 https://github.com/nebosuke/tomcat.git &&
+git clone -b $BRANCH --depth 1 https://github.com/nebosuke/tomcat.git &&
 cd tomcat &&
 git log &&
 ant deploy &&
-cd /work/tomcat/output &&
-mv build apache-tomcat-7.0.93 &&
-tar zcvf /dist/apache-tomcat-7.0.93-fix.tar.gz apache-tomcat-7.0.93
+cd .. &&
+cp -a tomcat/output/build/lib/*.jar apache-tomcat-${VERSION}/lib &&
+mv apache-tomcat-${VERSION} apache-tomcat-${BRANCH} &&
+tar zcvf /dist/apache-tomcat-${BRANCH}.tar.gz apache-tomcat-${BRANCH}
--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+if [ "x$BRANCH" = "x" ]; then
+    export BRANCH=7.0.94-p1
+fi
+if [ "x$VERSION" = "x" ]; then
+    export VERSION=7.0.94
+fi
+
 docker build -t tomcat7-builder .
-docker run -it -v `pwd`/dist:/dist --rm tomcat7-builder
+docker run -e BRANCH=$BRANCH -e VERSINO=$VERSION -it -v `pwd`/dist:/dist --rm tomcat7-builder
 
